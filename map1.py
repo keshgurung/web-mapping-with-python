2b3cab52cfa3b67d5bf71a80fa0ceaee7d16e806
keshgurung <k_grg@hotmail.com>
2022-05-30 16:32:11 +0100

    reading json file and adding style with lambda function

--- map1.py
+++ map1.py
@@ -31,7 +31,13 @@ fg = folium.FeatureGroup(name="my map")
 
 for lt, ln, el, name in zip(lat, lan, elev, name):
     iframe = folium.IFrame(html=html % (name,name,el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), radius=8 ,fill_color=color_producer(el), color='grey', fill_opacity=0.7))
+#folium.Circlemaker = gives circle icon, radius = size of circle, fill_color = inside the circle, color = circle border, opacity = to show the fill_color
+
+#add a child that creates a polygon around country and reads json file.
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if
+10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 
 map.add_child(fg)
 
