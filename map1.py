4533edc75ef2e733c7440fb823585ccf4461a0bd
keshgurung <k_grg@hotmail.com>
2022-05-30 14:04:09 +0100

    adding extra dynamic popup message

--- map1.py
+++ map1.py
@@ -5,6 +5,7 @@ data = pandas.read_csv('Volcanoes.txt')
 # getting values from colums of data i.e lat and lon,
 lat = list(data['LAT'])
 lan = list(data['LON'])
+elev = list(data['ELEV'])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain") 
 
@@ -12,8 +13,8 @@ fg = folium.FeatureGroup(name="my map")
 
 # we need to iterate 1 to 1 corresponding from lat and lon, we use zip() in our looop
 
-for lt, ln in zip(lat, lan):
-    fg.add_child(folium.Marker(location=[lt,ln], popup="hi, i am a marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lan, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(el)+' m', icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
